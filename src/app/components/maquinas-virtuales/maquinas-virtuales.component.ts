fc80e68a46d21ab3094222744df71c759c620657
facu <facundoduran31@gmail.com>
2023-09-15 11:59:54 -0300

    Borrar maquinas virtuales

--- src/app/components/maquinas-virtuales/maquinas-virtuales.component.ts
+++ src/app/components/maquinas-virtuales/maquinas-virtuales.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MaquinaVirtual } from '../../model/MaquinaVirtual';
 import { MaquinasVirtualesService } from '../../services/maquinas-virtuales.service';
 import { Router } from '@angular/router';
+import { ModalContentComponent } from '../modal-content/modal-content.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-maquinas-virtuales',
@@ -11,10 +13,37 @@ import { Router } from '@angular/router';
 export class MaquinasVirtualesComponent implements OnInit {
   maquinasVirtuales: MaquinaVirtual[] = [];
 
-  constructor(private maquinasService: MaquinasVirtualesService, private router:Router) { }
+  constructor(private maquinasService: MaquinasVirtualesService, private router:Router, private modalService:NgbModal) { }
 
   ngOnInit(): void {
     this.obtenerMaquinasVirtuales();
+  }
+
+  openModal(maquinaVirtual:MaquinaVirtual) {
+    const modalRef = this.modalService.open(ModalContentComponent);
+    modalRef.componentInstance.name = '¿Seguro que desea eliminar la máquina virtual?';
+    modalRef.componentInstance.message = 'Confirme la acción antes de continuar.';
+    modalRef.componentInstance.type = 'confirm';
+    modalRef.componentInstance.buttonText = 'Eliminar';
+    modalRef.componentInstance.buttonClass = 'btn-danger';
+    modalRef.result.then((result) => {
+      if (result === 'Eliminar') {
+        this.borrarMaquinaVirtual(maquinaVirtual);
+        this.router.navigate(['/']);
+      }
+    });
+  }
+
+  borrarMaquinaVirtual(maquinaVirtual:MaquinaVirtual): void {
+    this.maquinasService.eliminarMaquinaVirtual(maquinaVirtual.idMaquinaVirtual).subscribe(
+      (maquinaVirtual) => {
+        console.log(maquinaVirtual)
+        this.obtenerMaquinasVirtuales();
+      },
+      (error) => {
+        console.log('Error al borrar la máquina virtual:', error);
+      }
+    );
   }
 
   obtenerMaquinasVirtuales(): void {
