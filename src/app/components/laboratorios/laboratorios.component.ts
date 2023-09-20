2cc5c77cfe631b0a00f6d52710bb90e3c7af86d4
facu <facundoduran31@gmail.com>
2023-09-20 15:35:59 -0300

    Metodos openModal y borrarLaboratorio

--- src/app/components/laboratorios/laboratorios.component.ts
+++ src/app/components/laboratorios/laboratorios.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Laboratiorio } from 'src/app/model/Laboratorio';
+import { ModalContentComponent } from '../modal-content/modal-content.component';
 
 @Component({
   selector: 'app-laboratorios',
@@ -8,11 +10,25 @@ import { Laboratiorio } from 'src/app/model/Laboratorio';
 })
 export class LaboratoriosComponent {
 
-  constructor() { }
+  constructor(private modalService:NgbModal) { }
 
   laboratorios:Laboratiorio[] = [];
 
   openModal(laboratorio:Laboratiorio) {
+    const modalRef = this.modalService.open(ModalContentComponent);
+    modalRef.componentInstance.name = 'Confirme la acción antes de continuar.';
+    modalRef.componentInstance.message = '¿Seguro que desea eliminar la máquina virtual?';
+    modalRef.componentInstance.type = 'confirm';
+    modalRef.componentInstance.buttonText = 'Eliminar';
+    modalRef.componentInstance.buttonClass = 'btn-danger';
+    modalRef.result.then((result) => {
+      if (result === 'Eliminar') {
+        this.borrarLaboratorio(laboratorio);
+      }
+    });
+  }
 
+  borrarLaboratorio(laboratorio:Laboratiorio): void {
+    console.log(laboratorio)
   }
 }
