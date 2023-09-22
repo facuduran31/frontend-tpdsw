e7122f215afe88bdd5cefca93354f85eebac2876
facu <facundoduran31@gmail.com>
2023-09-22 11:42:07 -0300

    Model computadora

---
+++ src/app/model/Computadora.ts
@@ -0,0 +1,7 @@
+export interface Laboratorio {
+    idComputadora: number;
+    procesador: string;
+    descripcionRam: string;
+    descripcionAlmacenamiento: string;
+    laboratorio_idLaboratorio: number;
+}
