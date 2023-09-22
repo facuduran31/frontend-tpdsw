298bdaebc0bc282a9147ac3a9ac6bd18bd944b22
facu <facundoduran31@gmail.com>
2023-09-22 11:40:04 -0300

    Estructura card component

--- src/app/components/ver-laboratorio/ver-laboratorio.component.ts
+++ src/app/components/ver-laboratorio/ver-laboratorio.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Laboratorio } from 'src/app/model/Laboratorio';
+import { LaboratorioService } from 'src/app/services/laboratorios.service';
 
 @Component({
   selector: 'app-ver-laboratorio',
   templateUrl: './ver-laboratorio.component.html',
   styleUrls: ['./ver-laboratorio.component.css']
 })
-export class VerLaboratorioComponent {
+export class VerLaboratorioComponent implements OnInit {
+  @Input() computadoras: Laboratorio[] = [];
+
+  constructor() { }
+  
+  ngOnInit(): void {
+    
+  }
 
 }
