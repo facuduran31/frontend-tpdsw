08ce1cf5049638c06aefc827f4ac7a2567141f67
facu <facundoduran31@gmail.com>
2023-12-11 14:40:07 -0300

    Ver requerimiento

--- src/app/components/ver-requerimiento/ver-requerimiento.component.ts
+++ src/app/components/ver-requerimiento/ver-requerimiento.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Laboratorio } from 'src/app/model/Laboratorio';
 import { Requerimiento } from 'src/app/model/Requerimiento';
+import { LaboratorioService } from 'src/app/services/laboratorios.service';
 import { RequerimientosService } from 'src/app/services/requerimientos.service';
 
 @Component({
@@ -11,17 +13,53 @@ import { RequerimientosService } from 'src/app/services/requerimientos.service';
 export class VerRequerimientoComponent implements OnInit {
 
   idRequerimiento: number = 0;
-  requerimiento?: Requerimiento;
+  laboratorios: Laboratorio[] = [];
+  requerimiento:Requerimiento = {
+    idRequerimiento: null,
+    tipoRequerimiento: '',
+    tipoReserva: '',
+    estado: '',
+    comentarioEncargado: '',
+    legajoEncargado: null,
+    legajoDocente: null,
+    hdmi: false,
+    vga: false,
+    mouse: false,
+    idLaboratorio: null,
+    descripcion: '',
+    materia: '',
+    fechaInicio: null,
+    fechaFin: null,
+    horaInicio: null,
+    horaFin: null,
+    proyector: false,
+    zapatilla: false,
+    docente: ''
+  };
 
-  constructor(private requerimientosService: RequerimientosService, private route:ActivatedRoute) { }
+  constructor(private requerimientosService: RequerimientosService, private laboratoriosService:LaboratorioService, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
     const routeSnapshot = this.route.snapshot;
     this.idRequerimiento = routeSnapshot.params['id'];
     this.requerimientosService.getRequerimiento(this.idRequerimiento).subscribe(
       data => {
+        if (data != null){
+          this.requerimiento = data;
+          this.getLaboratorios();
+        }
+      },
+      err => {
+        console.error(err);
+      }
+    );
+  }
+
+  getLaboratorios(){
+    this.laboratoriosService.getLaboratorios().subscribe(
+      data => {
         if (data != null)
-        this.requerimiento = data;
+        this.laboratorios = data;
       },
       err => {
         console.error(err);
