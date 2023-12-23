5bf0975fe5d0b17edd6e8b06da9dc32429826c60
facu <facundoduran31@gmail.com>
2023-12-23 15:33:36 -0300

    navbar adaptado a tipo de usuario

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DocentesService } from 'src/app/services/docentes.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
-  constructor(private router:Router) {}
+  isEncargado: boolean = false;
+
+  constructor(private router:Router, private docentesService:DocentesService) {}
+
+  ngOnInit(): void {
+
+    let legajo = localStorage.getItem('legajo');
+    if(legajo){ 
+      this.docentesService.getDocenteByLegajo(legajo).subscribe(
+        res => {
+          console.log(res)
+          if(res){
+            if(res.tipoUsuario == 'Encargado'){
+              this.isEncargado = true;
+            }
+          }
+        },
+        err => console.log(err)
+      );
+    } 
+  }
 
   logout() {
 
